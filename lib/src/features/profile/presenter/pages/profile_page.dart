05270183ef19f608f83828983d126c01270036e2
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-05-14 22:25:25 -0300

    add user info to avatar widget

--- lib/src/features/profile/presenter/pages/profile_page.dart
+++ lib/src/features/profile/presenter/pages/profile_page.dart
@@ -17,9 +17,11 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   final _formKey = GlobalKey<FormState>();
 
-  final _nameController = TextEditingController(text: 'Zé da Manga');
+  final _nameController = TextEditingController(text: 'Papi da Silva');
   final _emailController = TextEditingController(text: 'teste@gmail.com');
-  final _addressController = TextEditingController(text: 'Rua Teste, 123, Taubaté');
+  final _addressController = TextEditingController(
+    text: 'Rua Teste, 123, Taubaté',
+  );
   final _passwordController = TextEditingController(text: 'P@ssw0rd');
 
   @override
@@ -61,13 +63,13 @@ class _ProfilePageState extends State<ProfilePage> {
           key: _formKey,
           child: Column(
             children: [
-              AvatarWidget(onTap: _changeProfilePicture,),
+              AvatarWidget(onTap: _changeProfilePicture,
+                userName: _nameController.text,
+                userEmail: _emailController.text,
+              ),
               Padding(
-                padding: const EdgeInsets.symmetric(vertical:8.0),
-                child: Divider(
-                  thickness: 1.0,
-                  color: AppColors.gray,
-                ),
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Divider(thickness: 1.0, color: AppColors.gray),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -77,38 +79,57 @@ class _ProfilePageState extends State<ProfilePage> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Dados cadastrais', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                      Text('Mantenha sempre seus dados atualizados.', style: TextStyle(fontSize: 16, color: AppColors.textColor),),
+                      Text(
+                        'Dados cadastrais',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Mantenha sempre seus dados atualizados.',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: AppColors.textColor,
+                        ),
+                      ),
                     ],
-                  )),
+                  ),
+                ),
               ),
-              SizedBox(height: 10,),
+              SizedBox(height: 10),
               GenericInput(
                 hintText: 'Nome completo',
                 controller: _nameController,
-                validator: (value) =>
-                    value == null || value.isEmpty ? 'Digite seu nome' : null,
+                validator:
+                    (value) =>
+                        value == null || value.isEmpty
+                            ? 'Digite seu nome'
+                            : null,
               ),
               GenericInput(
                 hintText: 'E-mail',
                 isEnabled: false,
                 controller: _emailController,
-                validator: (value) =>
-                    value == null || value.isEmpty ? 'Digite seu nome' : null,
+                validator:
+                    (value) =>
+                        value == null || value.isEmpty
+                            ? 'Digite seu nome'
+                            : null,
               ),
               GenericInput(
                 controller: _addressController,
                 hintText: 'Endereço',
-                validator: (value) =>
-                    value == null || value.isEmpty ? 'Digite seu endereço' : null,
+                validator:
+                    (value) =>
+                        value == null || value.isEmpty
+                            ? 'Digite seu endereço'
+                            : null,
               ),
               const SizedBox(height: 16),
               Padding(
-                padding: const EdgeInsets.symmetric(vertical:10.0),
-                child: Divider(
-                  thickness: 1.0,
-                  color: AppColors.gray,
-                ),
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Divider(thickness: 1.0, color: AppColors.gray),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -118,12 +139,25 @@ class _ProfilePageState extends State<ProfilePage> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Alterar senha', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                      Text('Sua senha deve conter 8 caracteres, um número, uma letra maíuscula, uma letra minúscula e um caractere especial (!@#\$%&*).', style: TextStyle(fontSize: 16, color: AppColors.textColor),),
+                      Text(
+                        'Alterar senha',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Sua senha deve conter 8 caracteres, um número, uma letra maíuscula, uma letra minúscula e um caractere especial (!@#\$%&*).',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: AppColors.textColor,
+                        ),
+                      ),
                     ],
-                  )),
+                  ),
+                ),
               ),
-              SizedBox(height: 10,),
+              SizedBox(height: 10),
               PasswordInput(
                 controller: _passwordController,
                 hintText: 'Nova Senha',
--- lib/src/features/profile/presenter/widgets/avatar_widget.dart
+++ lib/src/features/profile/presenter/widgets/avatar_widget.dart
@@ -2,33 +2,57 @@ import 'package:arenaz_mobile_v2/src/features/shared/colors/colors.dart';
 import 'package:flutter/material.dart';
 
 class AvatarWidget extends StatelessWidget {
-final void Function()? onTap;
+  final String userName;
+  final String userEmail;
+  final void Function()? onTap;
 
-  const AvatarWidget({super.key, this.onTap});
+  const AvatarWidget({
+    super.key,
+    this.onTap,
+    required this.userName,
+    required this.userEmail,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return  Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                   CircleAvatar(
-                    backgroundColor: AppColors.secondaryColor,
-                    radius: 60,
-                    child: Image.asset('assets/images/arenaz_logo.png'),
+    return Column(
+      children: [
+        Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            CircleAvatar(
+              backgroundColor: AppColors.secondaryColor,
+              radius: 60,
+              child: Image.asset('assets/images/arenaz_logo.png'),
+            ),
+            Positioned(
+              bottom: 4,
+              right: 4,
+              child: GestureDetector(
+                onTap: onTap,
+                child: const CircleAvatar(
+                  radius: 18,
+                  backgroundColor: AppColors.primaryColor,
+                  child: Icon(
+                    Icons.camera_alt,
+                    size: 18,
+                    color: AppColors.defaultBlack,
                   ),
-                  Positioned(
-                    bottom: 4,
-                    right: 4,
-                    child: GestureDetector(
-                      onTap: onTap,
-                      child: const CircleAvatar(
-                        radius: 18,
-                        backgroundColor: AppColors.primaryColor,
-                        child: Icon(Icons.camera_alt, size: 18, color: AppColors.defaultBlack),
-                      ),
-                    ),
-                  ),
-                ],
-              );
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 12),
+        Text(
+          userName,
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          userEmail,
+          style: TextStyle(fontSize: 16, color: AppColors.textColor),
+        ),
+      ],
+    );
   }
 }
